15f05724a1c74dc579d0bbb8d2ee2df18fed42d9
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-08-27 11:54:26 -0400

    fixed rendering on home page and ingredients page

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Router, Link, Route, useNavigate } from 'react-router-dom';
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Router, Link, Route, useNavigate } from "react-router-dom";
+import AOS from "aos";
+import { resetRecipes } from "../../store/allRecipesSlice";
 
 const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   AOS.init();
 
+  useEffect(() => {
+    dispatch(resetRecipes());
+  }, []);
+
   const handleGetStarted = () => {
-    navigate('/ingredients');
+    navigate("/ingredients");
   };
 
   return (
     <div className="home-container">
-      <h1 className="title" data-aos="zoom-in">Planty!</h1>
+      <h1 className="title" data-aos="zoom-in">
+        Planty!
+      </h1>
 
-      <p className="description" data-aos="zoom-in">Planty is a recipe search tool. Just enter the ingredients you have in your kitchen and Planty will give you delicious suggestions for any meal!
+      <div className="description" data-aos="zoom-in">
+        <p>
+          Planty is a recipe search tool. Just enter the ingredients you have in
+          your kitchen and Planty will give you delicious suggestions for any
+          meal!
+        </p>
+        <p>...and the best part? It's all plant based!</p>
+      </div>
 
-      <p>...and the best part? It's all plant based!</p></p>
-
-      <button className="get-started-button" onClick={handleGetStarted}>Let's get cooking!</button>
+      <button className="get-started-button" onClick={handleGetStarted}>
+        Let's get cooking!
+      </button>
     </div>
   );
 };
