ff57a1ac5c5b4dd77a90332560d0741f15219b22
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-08-27 10:47:06 -0400

    added reset search button w/ functionality

--- client/features/Recipes/Recommended.js
+++ client/features/Recipes/Recommended.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { resetRecipes } from '../../store/allRecipesSlice';
 
 const Recommended = () => {
   const recipes = useSelector((state) => state.recipes.recipes);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleReset = () => {
+    dispatch(resetRecipes());
+    navigate('/ingredients')
+  };
 
   return (
     <>
-      <div className="all-recipes-container">
         {recipes ? (
-          recipes.map((recipe) => (
-            <div className="recipe-container" key={recipe.id}>
-              <NavLink to={`/allrecipes/${recipe.id}`}>
-                <p id="recipe-name">{recipe.title}</p>
+          <>
+                <div className="all-recipes-container">
+            {recipes.map((recipe) => (
+              <div className="recipe-container" key={recipe.id}>
+                <NavLink to={`/allrecipes/${recipe.id}`}>
+                  <p id="recipe-name">{recipe.title}</p>
                   <img style={{ width: '200px' }} src={recipe.image} alt={recipe.name} />  
-              </NavLink>
-            </div>
-          ))
+                </NavLink>
+              </div>
+            ))}
+               </div>
+     
+            <button className="get-started-button" onClick={handleReset}>Back to Search</button>
+         
+          </>
         ) : (
-          <p>Loading recipes...</p>
+          <p>No recipes found! Try a different search.</p>
         )}
-      </div>
+   
     </>
   );
-}
+        } 
 
 export default Recommended;
