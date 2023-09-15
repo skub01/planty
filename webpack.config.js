b5ffa764f564f5f8b38a3213b25678e749f04928
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-15 10:56:56 -0400

    resolve babel error

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,8 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+
         },
-      },
     ],
   },
 };
